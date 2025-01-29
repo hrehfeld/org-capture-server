40502b45b3ba831c1fc2d378516b96da5d919d29
Hauke Rehfeld <git@haukerehfeld.de>
2025-01-29 15:29:10 +0100

    chg: bookmarklet: just direct to capture url and rely on response

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,9 +1,48 @@
 javascript:(function() {
   const title = document.title || "[untitled page]";
-  const body = function () {var html = ""; if (typeof window.getSelection != "undefined") {var sel = window.getSelection(); if (sel.rangeCount) {var container = document.createElement("div"); for (var i = 0, len = sel.rangeCount; i < len; ++i) {container.appendChild(sel.getRangeAt(i).cloneContents());} html = container.innerHTML;}} else if (typeof document.selection != "undefined") {if (document.selection.type == "Text") {html = document.selection.createRange().htmlText;}} var relToAbs = function (href) {var a = document.createElement("a"); a.href = href; var abs = a.protocol + "//" + a.host + a.pathname + a.search + a.hash; a.remove(); return abs;}; var elementTypes = [['a', 'href'], ['img', 'src']]; var div = document.createElement('div'); div.innerHTML = html; elementTypes.map(function(elementType) {var elements = div.getElementsByTagName(elementType[0]); for (var i = 0; i < elements.length; i++) {elements[i].setAttribute(elementType[1], relToAbs(elements[i].getAttribute(elementType[1])));}}); return div.innerHTML;}();
-  const url = 'http://{HOST}/capture?password={PASSWORD}&template=w&url=' + encodeURIComponent(location.href) + '&title=' + encodeURIComponent(title) + '&body=' + encodeURIComponent(body);
+  const body = function () {
+    var html = "";
+    if (typeof window.getSelection != "undefined") {
+      var sel = window.getSelection();
+      if (sel.rangeCount) {var container = document.createElement("div");
+        for (var i = 0, len = sel.rangeCount; i < len; ++i) {
+          container.appendChild(sel.getRangeAt(i).cloneContents());
+        }
+        html = container.innerHTML;
+      }
+    }
+    else if (typeof document.selection != "undefined") {
+      if (document.selection.type == "Text") {
+        html = document.selection.createRange().htmlText;
+      }
+    }
+    var relToAbs = function (href) {
+      var a = document.createElement("a");
+      a.href = href;
+      var abs = a.protocol + "//" + a.host + a.pathname + a.search + a.hash;
+      a.remove();
+      return abs;
+    };
+    var elementTypes = [['a', 'href'], ['img', 'src']];
+    var div = document.createElement('div');
+    div.innerHTML = html;
+    elementTypes.map(function(elementType) {
+      var elements = div.getElementsByTagName(elementType[0]);
+      for (var i = 0; i < elements.length; i++) {
+        elements[i].setAttribute(elementType[1], relToAbs(elements[i].getAttribute(elementType[1])));
+      }
+    });
+    return div.innerHTML;
+  }();
+  const url = 'http://{HOST}/capture?password={PASSWORD}&template=p&url=' + encodeURIComponent(location.href) + '&title=' + encodeURIComponent(title) + '&body=' + encodeURIComponent(body);
   console.log(url);
-  const oldUrl = location.href;
+  // const oldUrl = location.href;
   location.href = url;
-  window.setTimeout(function() {location.href = oldUrl;}, 500);
+  // var checkBack;
+  // checkBack = function {
+  //   if (location.hrehf != oldUrl) {
+  //     window.setTimeout(function() {location.href = oldUrl;}, 100);
+  //   }
+  // }
+  //window.fetch(url).then(function() { console.log('saved ' + url)});
 })()
